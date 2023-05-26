420af666b6f20c6a157c7a4d412b885360e9bc8c
SIROJIDDIN <jorisdandii@gmail.com>
2023-05-26 20:26:18 +0900

    feat: routerni qurib olish

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log("Web Serverni boshlash");
 const express = require("express");
 const app = express();
-
+const router = require("./router")
 //MongoDB connect
 const db = require("./server").db();
 const mongodb = require("mongodb");
@@ -18,5 +18,7 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4: Routing code
+app.use("/", router)
+
 
 module.exports = app;
