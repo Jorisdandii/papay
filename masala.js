281b83fca589dd9dafd13901aedf4f7177a199ef
SIROJIDDIN <jorisdandii@gmail.com>
2023-05-29 18:35:10 +0900

    fix: challenge CAN TOU SORT ME

--- masala.js
+++ masala.js
@@ -1,5 +1,19 @@
 
 
+
+
+
+// * CHALLENGE 4: CAN YOU SORT ME*/
+
+// const detail_list = [12, 23, null, 32, null, 34, null, null];
+// const result1 = moveNullsKeepOeder(detail_list);
+// console.log("result:", result1);
+
+// function moveNullsKeepOeder(detail_list) {
+//   let sortNull = detail_list.sort((a, b) => a - b);
+//   return sortNull.sort();
+// }
+
 // ************** Dean & Belli *** 2023.05.27 *****************
 // Integral son berilgan bo‘lsa, uning kvadrat son ekanligini aniqlang:
 // n darajali nonlarni topish: n = 2;
